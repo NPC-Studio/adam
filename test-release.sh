6ebb13806039f3c82edfdeb44d5da8abfdff89f6
Jonathan Spira <jjspira@gmail.com>
2022-03-01 16:13:42 -0500

    get outta here!

--- test-release.sh
+++
@@ -1,4 +0,0 @@
-cargo build --release;
-cd ../../Gms2/SwordAndField;
-./../../Rust/adam/target/release/adam build;
-cd ../../Rust/adam;
--- test.sh
+++
@@ -1,4 +0,0 @@
-cargo build;
-cd ../../Gms2/SwordAndField;
-./../../Rust/adam/target/debug/adam build;
-cd ../../Rust/adam;

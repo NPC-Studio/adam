c85c1c9803a017c79a24beec07fab356d6bad9d1
Jonathan PC <jjspira@gmail.com>
2022-07-13 15:26:23 -0400

    disabling check on windows for a meeting

--- src/runner/check_options.rs
+++ src/runner/check_options.rs
@@ -9,6 +9,7 @@ pub struct CheckOptions {
 }
 
 /// Run the check option
+#[cfg(not(target_os = "windows"))]
 pub fn run_check(check_options: CheckOptions) -> Result<(), Output> {
     let current_dir = std::env::current_dir().unwrap();
     let path = current_dir.join(&check_options.path_to_run);
@@ -26,4 +27,30 @@ pub fn run_check(check_options: CheckOptions) -> Result<(), Output> {
     } else {
         Err(output)
     }
+}
+
+/// Run the check option
+#[cfg(target_os = "windows")]
+pub fn run_check(check_options: CheckOptions) -> Result<(), Output> {
+    Ok(())
+    // let current_dir = Utf8PathBuf::from_path_buf(std::env::current_dir().unwrap()).unwrap();
+    // let path = current_dir.join(&check_options.path_to_run);
+    // let cmd = format!(
+    //     r#"powershell -ExecutionPolicy RemoteSigned -File "{}"#,
+    //     path
+    // );
+    // let mut cmd = Command::new(&cmd);
+
+    // if let Some(d2u) = check_options.directory_to_use {
+    //     let dir_to_use = current_dir.join(&d2u);
+    //     cmd.current_dir(dir_to_use);
+    // }
+
+    // let output = cmd.output().expect("Failed to execute command");
+
+    // if output.status.success() {
+    //     Ok(())
+    // } else {
+    //     Err(output)
+    // }
 }

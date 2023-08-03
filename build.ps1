c17b2ec05c333f3ac0f11a832d6056b63efefac3
Jonathan PC <jjspira@gmail.com>
2023-08-03 12:49:53 -0400

    fixed path

--- build.ps1
+++ build.ps1
@@ -4,5 +4,5 @@ cargo build -r;
 # release it
 Move-Item `
 -Path ./target/release/adam.exe `
--Destination ./../Gms2/SwordAndField/tools/bin/adam.exe `
+-Destination ./../../Gms2/SwordAndField/tools/bin/adam.exe `
 -Force;
